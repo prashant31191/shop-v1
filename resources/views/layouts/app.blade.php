c9c9a188effbaf5dd0be5a80e1071fc0061216b0
Serghei Pogor <mediapronetsrl@gmail.com>
2019-08-16 20:11:01 +0300

    Add files via upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-info mb-5 mt-1 fixed-top">
 
       <!-- Brand -->
-      <a class="navbar-brand" href="/shop-v1-laravel/public/">
+      <a class="navbar-brand" href="/shop-v1/public/">
         <img src="http://icons.iconarchive.com/icons/designbolts/seo/256/Market-Analysis-icon.png" style="height: 32px;">
       </a>
     
@@ -30,48 +30,49 @@
       <ul class="navbar-nav">
         
         <li class="nav-item">
-          <a class="nav-link" href="/shop-v1-laravel/public/">Home</a>
+          <a class="nav-link" href="/shop-v1/public/">Home</a>
         </li>
+
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Social Accounts</a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="/shop-v1-laravel/public/socials/new">NEW</a>
-              <a class="dropdown-item" href="/shop-v1-laravel/public/socials/list">LIST</a>
+              <a class="dropdown-item" href="/shop-v1/public/socials/new">NEW</a>
+              <a class="dropdown-item" href="/shop-v1/public/socials/list">LIST</a>
             </div>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Social Platforms</a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="/shop-v1-laravel/public/socials/platforms/new">NEW</a>
-              <a class="dropdown-item" href="/shop-v1-laravel/public/socials/platforms/list">LIST</a>
+              <a class="dropdown-item" href="/shop-v1/public/socials/platforms/new">NEW</a>
+              <a class="dropdown-item" href="/shop-v1/public/socials/platforms/list">LIST</a>
             </div>
         </li>  
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Product</a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="/shop-v1-laravel/public/products/list">LIST</a>
-              <a class="dropdown-item" href="/shop-v1-laravel/public/import/AliExpress">Import from AliExpress</a>
-              {{-- <a class="dropdown-item" href="/shop-v1-laravel/public/import/Ebay">Import from Ebay</a> --}}
+              <a class="dropdown-item" href="/shop-v1/public/products/list">LIST</a>
+              <a class="dropdown-item" href="/shop-v1/public/import/AliExpress">Import from AliExpress</a>
+              {{-- <a class="dropdown-item" href="/shop-v1/public/import/Ebay">Import from Ebay</a> --}}
             </div>
         </li> 
         
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Region</a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="/shop-v1-laravel/public/listregion/countries">Country List</a>
-              <a class="dropdown-item" href="/shop-v1-laravel/public/listregion/regions">Region List</a>
-              <a class="dropdown-item" href="/shop-v1-laravel/public/listregion/cities">City List</a>
-              <a class="dropdown-item" href="/shop-v1-laravel/public/import/regions/5">Import</a>
+              <a class="dropdown-item" href="/shop-v1/public/listregion/countries">Country List</a>
+              <a class="dropdown-item" href="/shop-v1/public/listregion/regions">Region List</a>
+              <a class="dropdown-item" href="/shop-v1/public/listregion/cities">City List</a>
+              <a class="dropdown-item" href="/shop-v1/public/import/regions/5">Import</a>
             </div>
         </li> 
       </ul>
 
       <ul class="ml-auto mb-1">
         
-          <form method="post" class="form-inline" action="/shop-v1-laravel/public/import/Ebay">
+          <form method="post" class="form-inline" action="/shop-v1/public/import/Ebay">
             {{ csrf_field() }}
 
             <input class="form-control mr-sm-2" type="text" name="query" placeholder="Search Term">
